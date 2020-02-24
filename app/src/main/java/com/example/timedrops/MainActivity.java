3718c5357cc929551d804840b4a617e4dd2000f4
Xie_yr <33393578+Belief997@users.noreply.github.com>
2020-02-25 01:43:18 +0800

    暂存 按钮回调

--- app/src/main/java/com/example/timedrops/MainActivity.java
+++ app/src/main/java/com/example/timedrops/MainActivity.java
@@ -1,27 +1,42 @@
 package com.example.timedrops;
 
 import android.os.Bundle;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
+import android.view.Menu;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.navigation.NavigationView;
-
-import androidx.drawerlayout.widget.DrawerLayout;
-
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-
-import android.view.Menu;
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
+    TextView textView1;
+    Button button;
+
+    @Override
+    protected void onCreat(Bundle saveInstanceState){
+        super.onCreate(saveInstanceState);
+        setContentView(R.layout.activity_main);
+        textView1=(TextView)findViewById(R.id.textView1);
+        button=(Button)findViewById(R.id.button);
+        button.setOnClickListener(new L1);
+    }
+
+    class L1 implements View.OnClickListener{
+        @Override
+        public void onClick(View view){
+            textView1.setText("hhh changed");
+        }
+    }
 
     private AppBarConfiguration mAppBarConfiguration;
 
@@ -66,4 +81,8 @@ public class MainActivity extends AppCompatActivity {
         return NavigationUI.navigateUp(navController, mAppBarConfiguration)
                 || super.onSupportNavigateUp();
     }
+
+
+
+
 }
